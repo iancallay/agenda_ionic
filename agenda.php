1372fdcf23554cd35a34e196b058f97c106c67c1
Ivan Ancallay <ivan.ancallay@gmail.com>
2025-01-21 19:52:40 -0500

    archivo config.php

---
+++ agenda.php
@@ -0,0 +1,9 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Credentials:true');
+header('Access-Control-Allow-Methods: PUT,GET,POST,DELETE,OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization, Accept, X-Requested-With, x-xsrf-token');
+header('ContentType:application/json; charset=utf-8');
+
+$post = json_decode(file_get_contents('php://input'), true);
---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST', 'www.ioasystem.com');
+define('DB_USER', 'ioasyste_agenda');
+define('DB_PASS', 'Agenda2425');
+define('DB_NAME', 'ioasyste_agenda');
+
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
